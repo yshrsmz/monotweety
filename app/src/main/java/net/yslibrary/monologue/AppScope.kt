8c3b476cdd3b3e67622e40cc421553e7bff9a287
yshrsmz <the.phantom.bane@gmail.com>
2016-09-24 18:49:15 +0900

    add scope annotations for Dagger

---
+++ app/src/main/java/net/yslibrary/monologue/AppScope.kt
@@ -0,0 +1,10 @@
+package net.yslibrary.monologue
+
+import javax.inject.Scope
+
+/**
+ * Created by yshrsmz on 2016/09/24.
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AppScope

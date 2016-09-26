53dcd9e98ca649192923832d1a8ca850dfaf769b
yshrsmz <the.phantom.bane@gmail.com>
2016-09-26 19:32:24 +0900

    add commands for NotificationService

--- app/src/main/java/net/yslibrary/monotweety/notification/NotificationService.kt
+++ app/src/main/java/net/yslibrary/monotweety/notification/NotificationService.kt
@@ -7,12 +7,28 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Binder
 import android.os.IBinder
+import android.support.annotation.StringDef
 import timber.log.Timber
 
 class NotificationService : Service() {
 
   companion object {
     const val ACTION = "net.yslibrary.monotweety.notification.NotificationService.Action"
+
+    const val KEY_COMMAND = "notification_command"
+
+    const val COMMAND_SHOW_NOTIFICATION = "net.yslibrary.monotweety.notification.NotificationService.ShowNotification"
+    const val COMMAND_HIDE_NOTIFICATION = "net.yslibrary.monotweety.notification.NotificationService.HideNotification"
+    const val COMMAND_DIRECT_TWEET = "net.yslibrary.monotweety.notification.NotificationService.DirectTweet"
+    const val COMMAND_SHOW_TWEET_DIALOG = "net.yslibrary.monotweety.notification.NotificationService.ShowTweetDialog"
+
+    fun cammandIntent(@CommandType command: String): Intent {
+      val intent = Intent()
+      intent.action = ACTION
+      intent.putExtra(KEY_COMMAND, command)
+
+      return intent
+    }
   }
 
   val binder: IBinder by lazy { ServiceBinder() }
@@ -67,10 +83,33 @@ class NotificationService : Service() {
       get() = this@NotificationService
   }
 
+  @Retention(AnnotationRetention.SOURCE)
+  @StringDef(
+      COMMAND_SHOW_NOTIFICATION,
+      COMMAND_HIDE_NOTIFICATION,
+      COMMAND_DIRECT_TWEET,
+      COMMAND_SHOW_TWEET_DIALOG)
+  annotation class CommandType
+
   inner class NotificationCommandReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
+      val command = intent?.getStringExtra(KEY_COMMAND)
 
+      Timber.d("onReceive - command: $command")
+
+      command?.let {
+        when (it) {
+          COMMAND_SHOW_NOTIFICATION -> {
+          }
+          COMMAND_HIDE_NOTIFICATION -> {
+          }
+          COMMAND_DIRECT_TWEET -> {
+          }
+          COMMAND_SHOW_TWEET_DIALOG -> {
+          }
+        }
+      }
     }
   }
 }

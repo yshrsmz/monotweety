95f2a7110bfb19776581209b3c49f26d985a73bc
yshrsmz <the.phantom.bane@gmail.com>
2016-09-27 17:04:46 +0900

    inject dependency in SettingController

--- app/src/main/java/net/yslibrary/monotweety/setting/SettingController.kt
+++ app/src/main/java/net/yslibrary/monotweety/setting/SettingController.kt
@@ -5,14 +5,22 @@ import android.view.View
 import android.view.ViewGroup
 import net.yslibrary.monotweety.R
 import net.yslibrary.monotweety.base.ActionBarController
+import net.yslibrary.monotweety.base.HasComponent
 
 /**
  * Created by yshrsmz on 2016/09/24.
  */
-class SettingController : ActionBarController() {
+class SettingController : ActionBarController(), HasComponent<SettingComponent> {
+
+  override val component: SettingComponent by lazy {
+    getComponentProvider<SettingComponent.ComponentProvider>(activity)
+        .settingComponent(SettingViewModule())
+  }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
     val view = inflater.inflate(R.layout.controller_setting, container, false)
+
+    component.inject(this)
 
     return view
   }

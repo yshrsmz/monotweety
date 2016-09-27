9d430e73558b58395543716bd64b4ca594240371
yshrsmz <the.phantom.bane@gmail.com>
2016-09-27 17:03:09 +0900

    add UserDataModule

--- app/src/main/java/net/yslibrary/monotweety/UserComponent.kt
+++ app/src/main/java/net/yslibrary/monotweety/UserComponent.kt
@@ -2,12 +2,15 @@ package net.yslibrary.monotweety
 
 import dagger.Subcomponent
 import net.yslibrary.monotweety.base.di.UserScope
+import net.yslibrary.monotweety.data.UserDataModule
 
 /**
  * Created by yshrsmz on 2016/09/24.
  */
 @UserScope
-@Subcomponent
+@Subcomponent(
+    modules = arrayOf(UserDataModule::class)
+)
 interface UserComponent : AppModule.Provider {
 
   interface ComponentProvider {
---
+++ app/src/main/java/net/yslibrary/monotweety/data/UserDataModule.kt
@@ -0,0 +1,11 @@
+package net.yslibrary.monotweety.data
+
+import dagger.Module
+
+/**
+ * Created by yshrsmz on 2016/09/27.
+ */
+@Module
+class UserDataModule {
+
+}

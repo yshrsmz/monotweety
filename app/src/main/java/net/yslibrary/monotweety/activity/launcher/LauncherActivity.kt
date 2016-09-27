b51545034fc4fa80a93b33e622ec1e3396419561
yshrsmz <the.phantom.bane@gmail.com>
2016-09-27 17:05:16 +0900

    add LauncherActivity component

--- app/src/main/java/net/yslibrary/monotweety/activity/launcher/LauncherActivity.kt
+++ app/src/main/java/net/yslibrary/monotweety/activity/launcher/LauncherActivity.kt
@@ -1,15 +1,18 @@
 package net.yslibrary.monotweety.activity.launcher
 
+import android.os.Bundle
 import com.bluelinelabs.conductor.ChangeHandlerFrameLayout
 import com.bluelinelabs.conductor.Controller
 import net.yslibrary.monotweety.R
+import net.yslibrary.monotweety.activity.ActivityModule
 import net.yslibrary.monotweety.base.BaseActivity
+import net.yslibrary.monotweety.base.HasComponent
 import net.yslibrary.monotweety.splash.SplashController
 
 /**
  * Created by yshrsmz on 2016/09/24.
  */
-class LauncherActivity : BaseActivity() {
+class LauncherActivity : BaseActivity(), HasComponent<LauncherActivityComponent> {
 
   override val container: ChangeHandlerFrameLayout
     get() = findViewById(R.id.controller_container) as ChangeHandlerFrameLayout
@@ -19,4 +22,16 @@ class LauncherActivity : BaseActivity() {
 
   override val rootController: Controller
     get() = SplashController()
+
+  override val component: LauncherActivityComponent by lazy {
+    DaggerLauncherActivityComponent.builder()
+        .activityModule(ActivityModule(this))
+        .build()
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    component.inject(this)
+  }
 }
---
+++ app/src/main/java/net/yslibrary/monotweety/activity/launcher/LauncherActivityComponent.kt
@@ -0,0 +1,22 @@
+package net.yslibrary.monotweety.activity.launcher
+
+import dagger.Component
+import net.yslibrary.monotweety.AppComponent
+import net.yslibrary.monotweety.activity.ActivityModule
+import net.yslibrary.monotweety.base.di.ActivityScope
+import net.yslibrary.monotweety.login.LoginComponent
+import net.yslibrary.monotweety.splash.SplashComponent
+
+/**
+ * Created by yshrsmz on 2016/09/27.
+ */
+@ActivityScope
+@Component(
+    dependencies = arrayOf(AppComponent::class),
+    modules = arrayOf(ActivityModule::class)
+)
+interface LauncherActivityComponent : ActivityModule.Provider,
+                                      SplashComponent.ComponentProvider,
+                                      LoginComponent.ComponentProvider {
+  fun inject(activity: LauncherActivity)
+}

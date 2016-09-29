fc2ae12b64f1dd276b3beae6d7866578604a7d71
yshrsmz <the.phantom.bane@gmail.com>
2016-09-30 01:20:02 +0900

    expose NotificationEnabledManager

--- app/src/main/java/net/yslibrary/monotweety/UserComponent.kt
+++ app/src/main/java/net/yslibrary/monotweety/UserComponent.kt
@@ -3,6 +3,7 @@ package net.yslibrary.monotweety
 import dagger.Subcomponent
 import net.yslibrary.monotweety.base.di.UserScope
 import net.yslibrary.monotweety.data.UserDataModule
+import net.yslibrary.monotweety.setting.domain.NotificationEnabledManager
 
 /**
  * Created by yshrsmz on 2016/09/24.
@@ -12,6 +13,8 @@ import net.yslibrary.monotweety.data.UserDataModule
     modules = arrayOf(UserDataModule::class)
 )
 interface UserComponent : AppModule.Provider {
+
+  fun notificationEnabledManager(): NotificationEnabledManager
 
   interface ComponentProvider {
     fun userComponent(): UserComponent

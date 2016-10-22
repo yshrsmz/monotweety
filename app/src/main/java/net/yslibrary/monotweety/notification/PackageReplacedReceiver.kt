2dbe8028bf4e4b995f7e6ffd40ba6750f229d2ac
yshrsmz <the.phantom.bane@gmail.com>
2016-10-23 00:25:12 +0900

    receive package_replaced intent and restart service #21

---
+++ app/src/main/java/net/yslibrary/monotweety/notification/PackageReplacedReceiver.kt
@@ -0,0 +1,30 @@
+package net.yslibrary.monotweety.notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import net.yslibrary.monotweety.App
+import rx.Observable
+import timber.log.Timber
+
+/**
+ * Created by yshrsmz on 2016/10/22.
+ */
+class PackageReplacedReceiver : BroadcastReceiver() {
+
+  override fun onReceive(context: Context, intent: Intent) {
+    Timber.d("package replaced")
+
+    Observable.zip(
+        App.appComponent(context).notificationEnabledManager().get(),
+        App.appComponent(context).isLoggedIn().execute().toObservable(),
+        { enabled, loggedIn -> enabled && loggedIn })
+        .first()
+        .subscribe {
+          Timber.d("is logged in and notification enabled: $it")
+          if (it) {
+            context.startService(NotificationService.callingIntent(context))
+          }
+        }
+  }
+}

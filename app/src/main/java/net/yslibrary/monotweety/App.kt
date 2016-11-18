2d837047f9a43044cb6242428627201e0f430c2d
yshrsmz <the.phantom.bane@gmail.com>
2016-11-18 10:03:44 +0900

    check device’s os version before starting EditorTileService

--- app/src/main/java/net/yslibrary/monotweety/App.kt
+++ app/src/main/java/net/yslibrary/monotweety/App.kt
@@ -2,6 +2,7 @@ package net.yslibrary.monotweety
 
 import android.app.Application
 import android.content.Context
+import android.os.Build
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
 import net.yslibrary.monotweety.quicksetting.EditorTileService
@@ -62,8 +63,7 @@ open class App : Application() {
     appComponent(this).inject(this)
     lifecycleCallbacks.onCreate()
 
-    // start Service for Quick Settings' Tile
-    startService(EditorTileService.callingIntent(this))
+    startEditorTileServiceIfNeeded()
 
     Timber.d("App#onCreate")
   }
@@ -72,6 +72,14 @@ open class App : Application() {
     super.onTerminate()
     Timber.d("App#onTerminate")
     lifecycleCallbacks.onTerminate()
+  }
+
+  fun startEditorTileServiceIfNeeded() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+      // quick settings is supported on Android N and up
+      // start Service for Quick Settings' Tile
+      startService(EditorTileService.callingIntent(this))
+    }
   }
 
 // FIXME: https://youtrack.jetbrains.com/issue/KT-14306

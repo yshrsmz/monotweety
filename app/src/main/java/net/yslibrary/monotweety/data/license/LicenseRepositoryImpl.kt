f6240e506019a0851b0a9d5cc06b2b03587ec2b7
yshrsmz <the.phantom.bane@gmail.com>
2017-01-09 20:59:51 +0900

    :arrow_up: licenseadapter 1.3.0

--- app/src/main/java/net/yslibrary/monotweety/data/license/LicenseRepositoryImpl.kt
+++ app/src/main/java/net/yslibrary/monotweety/data/license/LicenseRepositoryImpl.kt
@@ -3,8 +3,10 @@ package net.yslibrary.monotweety.data.license
 import net.yslibrary.licenseadapter.GitHubLicenseEntry
 import net.yslibrary.licenseadapter.LicenseEntry
 import net.yslibrary.licenseadapter.Licenses
+import net.yslibrary.licenseadapter.internal.BaseLicenseEntry
 import net.yslibrary.monotweety.base.di.UserScope
 import rx.Single
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -14,30 +16,36 @@ import javax.inject.Inject
 class LicenseRepositoryImpl @Inject constructor() : LicenseRepository {
   override fun get(): Single<List<LicenseEntry>> {
     return Single.fromCallable {
-      val list = listOf(
-          Licenses.noContent("Android SDK", "Google Inc.", "https://developer.android.com/sdk/terms.html"),
-          Licenses.noContent("Fabric", "Twitter", "https://fabric.io/terms"),
-          Licenses.fromGitHub("bluelinelabs/Conductor"),
-          Licenses.fromGitHub("hdodenhof/CircleImageView"),
+      val list = listOf<BaseLicenseEntry>(
+          Licenses.fromGitHubApacheV2("bluelinelabs/Conductor"),
+          Licenses.fromGitHubApacheV2("hdodenhof/CircleImageView"),
           Licenses.fromGitHub("gabrielemariotti/changeloglib", Licenses.LICENSE_APACHE_V2),
-          Licenses.fromGitHub("google/dagger"),
-          Licenses.fromGitHub("bumptech/glide", Licenses.FILE_NO_EXTENSION),
-          Licenses.fromGitHub("JakeWharton/RxBinding"),
-          Licenses.fromGitHub("JakeWharton/timber"),
-          Licenses.fromGitHub("JakeWharton/ThreeTenABP", Licenses.FILE_TXT),
-          Licenses.fromGitHub("f2prateek/rx-preferences"),
-          Licenses.fromGitHub("pushtorefresh/storio"),
+          Licenses.fromGitHubApacheV2("google/dagger"),
+          Licenses.fromGitHubApacheV2("bumptech/glide", Licenses.FILE_NO_EXTENSION),
+          Licenses.fromGitHubApacheV2("JakeWharton/RxBinding"),
+          Licenses.fromGitHubApacheV2("JakeWharton/timber"),
+          Licenses.fromGitHubApacheV2("JakeWharton/ThreeTenABP", Licenses.FILE_TXT),
+          Licenses.fromGitHubApacheV2("f2prateek/rx-preferences"),
+          Licenses.fromGitHubApacheV2("pushtorefresh/storio"),
           GitHubLicenseEntry(Licenses.NAME_APACHE_V2, "ReactiveX/RxJava", "1.x", null, Licenses.FILE_NO_EXTENSION),
           GitHubLicenseEntry(Licenses.NAME_APACHE_V2, "ReactiveX/RxAndroid", "1.x", null, Licenses.FILE_NO_EXTENSION),
-          Licenses.fromGitHub("sockeqwe/AdapterDelegates", Licenses.FILE_NO_EXTENSION),
-          Licenses.fromGitHub("twitter/twitter-text", Licenses.FILE_NO_EXTENSION),
-          Licenses.fromGitHub("twitter/twitter-kit-android", Licenses.FILE_NO_EXTENSION),
-          Licenses.fromGitHub("yshrsmz/LicenseAdapter", Licenses.FILE_NO_EXTENSION),
-          Licenses.fromGitHub("yshrsmz/RxEventBus", Licenses.FILE_NO_EXTENSION))
+          Licenses.fromGitHubApacheV2("sockeqwe/AdapterDelegates", Licenses.FILE_NO_EXTENSION),
+          Licenses.fromGitHubApacheV2("twitter/twitter-text", Licenses.FILE_NO_EXTENSION),
+          Licenses.fromGitHubApacheV2("twitter/twitter-kit-android", Licenses.FILE_NO_EXTENSION),
+          Licenses.fromGitHubApacheV2("yshrsmz/LicenseAdapter", Licenses.FILE_NO_EXTENSION),
+          Licenses.fromGitHubApacheV2("yshrsmz/RxEventBus", Licenses.FILE_NO_EXTENSION))
 
-      Licenses.load(list)
-
-      list
+      // sort github hosted repos first
+      list.sortedBy { it.name.toLowerCase(Locale.ENGLISH) }
+          .let {
+            val mutable = it.toMutableList()
+            mutable.addAll(0, listOf(
+                Licenses.noContent("Android SDK", "Google Inc.", "https://developer.android.com/sdk/terms.html"),
+                Licenses.noContent("Fabric", "Twitter", "https://fabric.io/terms")
+            ))
+            mutable.toList()
+          }
+          .apply { Licenses.load(this) }
     }
   }
 }

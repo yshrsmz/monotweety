872e7b8b3cb535bf311a836a9f502f90a9dc846f
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2017-09-13 16:23:09 +0900

    add stetho

--- app/src/debug/java/net/yslibrary/monotweety/DebugAppLifecycleCallbacks.kt
+++ app/src/debug/java/net/yslibrary/monotweety/DebugAppLifecycleCallbacks.kt
@@ -2,6 +2,7 @@ package net.yslibrary.monotweety
 
 import android.app.NotificationManager
 import android.content.Context
+import com.facebook.stetho.Stetho
 import timber.log.Timber
 
 class DebugAppLifecycleCallbacks(
@@ -9,7 +10,16 @@ class DebugAppLifecycleCallbacks(
     notificationManager: NotificationManager
 ) : AppLifecycleCallbacks(context, notificationManager) {
 
+  override fun onCreate() {
+    super.onCreate()
+    initStetho()
+  }
+
   override fun initTimber() {
     Timber.plant(Timber.DebugTree())
+  }
+
+  fun initStetho() {
+    Stetho.initializeWithDefaults(context)
   }
 }

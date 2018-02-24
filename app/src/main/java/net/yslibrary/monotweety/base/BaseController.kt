82d7a43a77c8564250bf192c32b556ed0e91d03e
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-02-24 21:44:30 +0900

    use ToastCompat to fix bug in 7.1

--- app/src/main/java/net/yslibrary/monotweety/base/BaseController.kt
+++ app/src/main/java/net/yslibrary/monotweety/base/BaseController.kt
@@ -1,11 +1,13 @@
 package net.yslibrary.monotweety.base
 
+import android.os.Build
 import android.support.annotation.StringRes
 import android.widget.Toast
 import com.bluelinelabs.conductor.rxlifecycle2.RxController
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
+import me.drakeet.support.toast.ToastCompat
 import net.yslibrary.monotweety.analytics.Analytics
 import net.yslibrary.monotweety.base.di.Names
 import javax.inject.Inject
@@ -34,11 +36,25 @@ abstract class BaseController : RxController() {
 
   fun showSnackBar(message: String) = (activity as BaseActivity).showSnackBar(message)
 
-  fun toast(message: String): Toast? = applicationContext?.let { Toast.makeText(it, message, Toast.LENGTH_SHORT) }
+  fun toast(message: String): Toast? = applicationContext?.let {
+    if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+      ToastCompat.makeText(it, message, Toast.LENGTH_SHORT)
+    } else {
+      Toast.makeText(it, message, Toast.LENGTH_SHORT)
+    }
 
-  fun toastLong(message: String): Toast? = applicationContext?.let { Toast.makeText(it, message, Toast.LENGTH_LONG) }
+  }
+
+  fun toastLong(message: String): Toast? = applicationContext?.let {
+    if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+      ToastCompat.makeText(it, message, Toast.LENGTH_SHORT)
+    } else {
+      Toast.makeText(it, message, Toast.LENGTH_LONG)
+    }
+  }
 
   fun getString(@StringRes id: Int): String = applicationContext?.getString(id) ?: ""
 
-  fun getString(@StringRes id: Int, vararg formatArgs: Any): String = applicationContext?.getString(id, *formatArgs) ?: ""
+  fun getString(@StringRes id: Int, vararg formatArgs: Any): String = applicationContext?.getString(id, *formatArgs)
+      ?: ""
 }
